ef44e26e415556d48f2d05bb1accd296e67cc34d
Horfi <grudka@interia.pl>
2022-05-25 13:54:55 +0200

    Simple fighting die animation | health

--- First RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ First RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -9,11 +9,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage;
 
         Transform combatTarget;
+        float timeScienceLastAtack = 0;
 
         private void Update()
         {
+            timeScienceLastAtack += Time.deltaTime;
             if (combatTarget == null) return;
             if (!GetIsInRange())
             {
@@ -28,7 +32,20 @@ namespace RPG.Combat
 
         private void Attackbehaviour()
         {
-            GetComponent<Animator>().SetTrigger("attack");
+            if (timeScienceLastAtack > timeBetweenAttacks)
+            {
+                // trigger hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeScienceLastAtack = 0;
+
+            }
+
+        }
+        // Animation Event
+        void Hit()
+        {
+            Health healthComponent = combatTarget.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -44,12 +61,9 @@ namespace RPG.Combat
 
         public void Cancel()
         {
+            GetComponent<Animator>().SetTrigger("stopAttack");
             combatTarget = null;
         }
-        // Animation Event
-        void Hit()
-        {
 
-        }
     }
 }
---
+++ First RPG Project/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float healithPoints = 100f;
+
+        bool isDead = false;
+
+        public void TakeDamage(float damage)
+        {
+            healithPoints = Mathf.Max(healithPoints - damage, 0);
+            print(healithPoints);
+            if(healithPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
+    }
+}
